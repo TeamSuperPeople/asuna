ec75bbd844db1cd0bc1e10089c4347b849702415
Draylar <samuelhimes@gmail.com>
2020-03-27 20:18:03 -0500

    Initial commit

---
+++ src/main/java/tsp/asuna/Asuna.java
@@ -0,0 +1,10 @@
+package tsp.asuna;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Asuna implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/tsp/asuna/AsunaClient.java
@@ -0,0 +1,13 @@
+package tsp.asuna.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.EnvType;
+
+@Environment(EnvType.CLIENT)
+public class AsunaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
