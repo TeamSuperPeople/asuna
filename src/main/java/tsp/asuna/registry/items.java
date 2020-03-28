4146328e65bac2cd1405d935ece3b3cf8e6d73fd
Ljime <Ljime@users.noreply.github.com>
2020-03-27 21:38:25 -0400

    lol

---
+++ src/main/java/tsp/asuna/registry/items.java
@@ -0,0 +1,37 @@
+package tsp.asuna.registry;
+
+import net.minecraft.item.Item;
+
+import static javax.swing.plaf.metal.MetalToolBarUI.register;
+
+
+public class items {
+
+    // Spells
+    public static final Item MIASMA = register("miasma", new Item(new Item.Settings().group()));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
