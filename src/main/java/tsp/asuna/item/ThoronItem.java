e99251a5831ec492d49d5d7da060cbea23efb522
Draylar <samuelhimes@gmail.com>
2020-04-01 18:15:41 -0500

    :yeethaw:

--- src/main/java/tsp/asuna/item/ThoronItem.java
+++ src/main/java/tsp/asuna/item/ThoronItem.java
@@ -3,12 +3,9 @@ package tsp.asuna.item;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.item.TooltipContext;
-import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.particle.ParticleEffect;
-import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
@@ -17,11 +14,13 @@ import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import tsp.asuna.entities.ThoronEntity;
 
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ThoronItem extends Item {
     public ThoronItem(Settings settings) {
@@ -45,52 +44,20 @@ public class ThoronItem extends Item {
         user.getItemCooldownManager().set(this, 20);
 
         if (!world.isClient) {
-            ThoronEntity thoronEntity = new ThoronEntity(world, user);
-            ThoronEntity thoronEntity1 = new ThoronEntity(world, user);
-            ThoronEntity thoronEntity2 = new ThoronEntity(world, user);
-            ThoronEntity thoronEntity3 = new ThoronEntity(world, user);
-            ThoronEntity thoronEntity4 = new ThoronEntity(world, user);
-
-            thoronEntity.setProperties(user, user.pitch, user.yaw, 0F, 2, 0);
-            thoronEntity1.setProperties(user, user.pitch, user.yaw, 0F, 2, 0);
-            thoronEntity2.setProperties(user, user.pitch, user.yaw, 0F, 2, 0);
-            thoronEntity3.setProperties(user, user.pitch, user.yaw, 0F, 2, 0);
-            thoronEntity4.setProperties(user, user.pitch, user.yaw, 0F, 2, 0);
-
-            thoronEntity.setPos(user.getX(),user.getY()+2,user.getZ());
-            thoronEntity1.setPos(user.getX(),user.getY()+2,user.getZ());
-            thoronEntity2.setPos(user.getX(),user.getY()+2,user.getZ());
-            thoronEntity3.setPos(user.getX(),user.getY()+2,user.getZ());
-            thoronEntity4.setPos(user.getX(),user.getY()+2,user.getZ());
-
-            world.spawnEntity(thoronEntity);
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            // DO NOT KEEP THIS AT 500
+            // STOPSHIP: 4/1/2020 DOO NOT KEEP THIS
+            for (int i = 0; i < 500; i++) {
+                ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+                scheduledExecutorService.schedule(() -> {
+                    world.getServer().execute(() -> {
+                        ThoronEntity thoronEntity = new ThoronEntity(world, user);
+                        thoronEntity.setProperties(user, user.pitch, user.yaw, 0F, 2, 0);
+                        thoronEntity.setPos(user.getX(),user.getY()+2,user.getZ());
+                        world.spawnEntity(thoronEntity);
+                    });
+                }, i * 10, TimeUnit.MILLISECONDS);
+                // i * 10 = 1 per 10 ms, 500 entities = 500 * 10 ms total = 5 seconds
             }
-            world.spawnEntity(thoronEntity1);
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            world.spawnEntity(thoronEntity2);
-            try {
-                Thread.sleep(150);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            world.spawnEntity(thoronEntity3);
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            world.spawnEntity(thoronEntity4);
-
-
         }
 
         user.incrementStat(Stats.USED.getOrCreateStat(this));
