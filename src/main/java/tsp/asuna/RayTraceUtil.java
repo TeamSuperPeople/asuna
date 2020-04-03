9475aa829e535661ebd6bf12b650edf32473b272
Draylar <samuelhimes@gmail.com>
2020-04-02 22:21:35 -0500

    pig go fire

---
+++ src/main/java/tsp/asuna/RayTraceUtil.java
@@ -0,0 +1,79 @@
+package tsp.asuna;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public class RayTraceUtil {
+
+    public static Optional<Entity> getTargetedEntity(Entity entity, int maxDistance) {
+        if (entity == null) {
+            return Optional.empty();
+        } else {
+            Vec3d vec3d = entity.getCameraPosVec(1.0F);
+            Vec3d vec3d2 = entity.getRotationVec(1.0F).multiply((double)maxDistance);
+            Vec3d vec3d3 = vec3d.add(vec3d2);
+            Box box = entity.getBoundingBox().stretch(vec3d2).expand(1.0D);
+            int i = maxDistance * maxDistance;
+            Predicate<Entity> predicate = (entityx) -> {
+                return !entityx.isSpectator() && entityx.collides();
+            };
+            EntityHitResult entityHitResult = rayTrace(entity, vec3d, vec3d3, box, predicate, i);
+            if (entityHitResult == null) {
+                return Optional.empty();
+            } else {
+                return vec3d.squaredDistanceTo(entityHitResult.getPos()) > (double)i ? Optional.empty() : Optional.of(entityHitResult.getEntity());
+            }
+        }
+    }
+
+    public static EntityHitResult rayTrace(Entity entity, Vec3d vec3d, Vec3d vec3d2, Box box, Predicate<Entity> predicate, double d) {
+        World world = entity.world;
+        double e = d;
+        Entity entity2 = null;
+        Vec3d vec3d3 = null;
+        Iterator var12 = world.getEntities(entity, box, predicate).iterator();
+
+        while(true) {
+            while(var12.hasNext()) {
+                Entity entity3 = (Entity)var12.next();
+                Box box2 = entity3.getBoundingBox().expand((double)entity3.getTargetingMargin());
+                Optional<Vec3d> optional = box2.rayTrace(vec3d, vec3d2);
+                if (box2.contains(vec3d)) {
+                    if (e >= 0.0D) {
+                        entity2 = entity3;
+                        vec3d3 = (Vec3d)optional.orElse(vec3d);
+                        e = 0.0D;
+                    }
+                } else if (optional.isPresent()) {
+                    Vec3d vec3d4 = (Vec3d)optional.get();
+                    double f = vec3d.squaredDistanceTo(vec3d4);
+                    if (f < e || e == 0.0D) {
+                        if (entity3.getRootVehicle() == entity.getRootVehicle()) {
+                            if (e == 0.0D) {
+                                entity2 = entity3;
+                                vec3d3 = vec3d4;
+                            }
+                        } else {
+                            entity2 = entity3;
+                            vec3d3 = vec3d4;
+                            e = f;
+                        }
+                    }
+                }
+            }
+
+            if (entity2 == null) {
+                return null;
+            }
+
+            return new EntityHitResult(entity2, vec3d3);
+        }
+    }
+}
--- src/main/java/tsp/asuna/item/BolganoneItem.java
+++ src/main/java/tsp/asuna/item/BolganoneItem.java
@@ -2,31 +2,26 @@ package tsp.asuna.item;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.item.TooltipContext;
+import net.minecraft.client.render.debug.DebugRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.hit.EntityHitResult;
-import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.World;
-import tsp.asuna.entities.MiasmaEntity;
-import tsp.asuna.entities.ThoronEntity;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class BolganoneItem extends Item {
 
@@ -44,25 +39,23 @@ public class BolganoneItem extends Item {
         world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_ENDER_DRAGON_SHOOT, SoundCategory.NEUTRAL, 0.5F, 0.4F / (RANDOM.nextFloat() * 0.4F + 0.8F));
         user.getItemCooldownManager().set(this, 20);
 
-        HitResult hitResult = user.rayTrace(50D,100F,true);
         if (!world.isClient) {
-            if (hitResult.getType() == HitResult.Type.ENTITY) {
+            Optional<Entity> optionalEntioty = DebugRenderer.getTargetedEntity(user, 20);
 
-
-                EntityHitResult entityHitResult = ((EntityHitResult) hitResult);
-                Entity entity = entityHitResult.getEntity();
+            if (optionalEntioty.isPresent() && optionalEntioty.get() instanceof LivingEntity) {
+                LivingEntity entity = (LivingEntity) optionalEntioty.get();
                 entity.setOnFireFor(5);
 
 
                 user.getItemCooldownManager().set(this, 20);
 
-                for (int animationProgress = 0; animationProgress < 5; animationProgress++) {
+                for (AtomicInteger animationProgress = new AtomicInteger(); animationProgress.get() < 5; animationProgress.incrementAndGet()) {
                     ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
                     scheduledExecutorService.schedule(() -> {
                         world.getServer().execute(() -> {
-                            world.addParticle(ParticleTypes.FLAME, entity.getX() + Math.cos(animationProgress / 5f + 16), entity.getY() + animationProgress % 50 / 25f, entity.getZ() + Math.sin(animationProgress / 5f + 16), 5, 0, 0, 0);
+                            ((ServerWorld) world).spawnParticles(ParticleTypes.FLAME, entity.getX() + Math.cos(animationProgress.get() / 5f + 16), entity.getY() + animationProgress.get() % 50 / 25f, entity.getZ() + Math.sin(animationProgress.get() / 5f + 16), 5, 0f, 0f, 0f, 0);
                         });
-                    }, animationProgress * 100, TimeUnit.MILLISECONDS);
+                    }, animationProgress.get() * 100, TimeUnit.MILLISECONDS);
                     // i * 10 = 1 per 10 ms, 500 entities = 500 * 10 ms total = 5 seconds
                 }
             }
