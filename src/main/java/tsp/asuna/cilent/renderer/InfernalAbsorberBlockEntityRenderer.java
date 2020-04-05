9adb267cf4c1516d5d777691316fb8a3f1e28203
Draylar <samuelhimes@gmail.com>
2020-04-04 22:27:38 -0500

    fix mana crystal texture, add weird mana/lava renderer display

--- src/main/java/tsp/asuna/cilent/renderer/InfernalAbsorberBlockEntityRenderer.java
+++ src/main/java/tsp/asuna/cilent/renderer/InfernalAbsorberBlockEntityRenderer.java
@@ -15,7 +15,27 @@ public class InfernalAbsorberBlockEntityRenderer extends BlockEntityRenderer<Inf
 
     @Override
     public void render(InfernalAbsorberBlockEntity blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
-        LabelRenderer.renderLabelIfPresent(blockEntity, "Mana: " + blockEntity.getMana(), 0, matrices, vertexConsumers, 15728880);
-        LabelRenderer.renderLabelIfPresent(blockEntity, "Lava: " + blockEntity.getLava(), .5f, matrices, vertexConsumers, 15728880);
+//        LabelRenderer.renderLabelIfPresent(blockEntity, "▮▯▯▯▯▯▯▯▯▯", 0x29d0e3, 0, matrices, vertexConsumers, 15728880);
+//        LabelRenderer.renderLabelIfPresent(blockEntity, "▮▯▯▯▯▯▯▯▯▯", 0xf74d14, .5f, matrices, vertexConsumers, 15728880);
+
+        LabelRenderer.renderLabelIfPresent(blockEntity, getCursedDisplay(blockEntity.getMaxMana(), blockEntity.getMana()), 0x29d0e3, 0, matrices, vertexConsumers, 15728880);
+        LabelRenderer.renderLabelIfPresent(blockEntity,  getCursedDisplay(blockEntity.getMaxMana(), blockEntity.getLava()), 0xf74d14, .5f, matrices, vertexConsumers, 15728880);
+    }
+
+    public String getCursedDisplay(int max, int amount) {
+        String returnDisplay = "";
+        float manaProgress = amount / (float) max; // 0.0 -> 1.0
+        manaProgress *= 10; // 0.0 -> 10.0
+        int progress = (int) Math.floor(manaProgress);
+
+        for(int i = 0; i < progress; i++) {
+            returnDisplay += "▮";
+        }
+
+        for(int i = 0; i < (10 - progress); i++) {
+            returnDisplay += "▯";
+        }
+
+        return returnDisplay;
     }
 }
